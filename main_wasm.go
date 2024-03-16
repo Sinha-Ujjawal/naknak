472acd74413f928ee35af3a68267022fd41dc7d7
Sinha-Ujjawal <ujjawalsinhacool16021998@gmail.com>
2024-03-16 23:25:48 +0530

    Changes
    * Using tabs instead of spaces because gofmt is fucking annoying!

--- main_wasm.go
+++ main_wasm.go
@@ -1,36 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "syscall/js"
-    "naknak/naknak"
+	"fmt"
+	"naknak/naknak"
+	"syscall/js"
 )
 
 func encode(this js.Value, inputs []js.Value) any {
-    if len(inputs) != 1 {
-        return fmt.Sprintf("Expected one argument, got `%+v` arguments", inputs)
-    }
-    if inputs[0].Type() != js.TypeString     {
-        return "Expected a string argument"
-    }
-    text := inputs[0].String()
-    return naknak.Encode(text)
+	if len(inputs) != 1 {
+		return fmt.Sprintf("Expected one argument, got `%+v` arguments", inputs)
+	}
+	if inputs[0].Type() != js.TypeString {
+		return "Expected a string argument"
+	}
+	text := inputs[0].String()
+	return naknak.Encode(text)
 }
 
 func decode(this js.Value, inputs []js.Value) any {
-    if len(inputs) != 1 {
-        return fmt.Sprintf("Expected one argument, got `%+v` arguments", inputs)
-    }
-    if inputs[0].Type() != js.TypeString     {
-        return "Expected a string argument"
-    }
-    text := inputs[0].String()
-    return naknak.Decode(text)
+	if len(inputs) != 1 {
+		return fmt.Sprintf("Expected one argument, got `%+v` arguments", inputs)
+	}
+	if inputs[0].Type() != js.TypeString {
+		return "Expected a string argument"
+	}
+	text := inputs[0].String()
+	return naknak.Decode(text)
 }
 
 func main() {
-    c := make(chan struct{}, 0)
-    js.Global().Set("encode", js.FuncOf(encode))
-    js.Global().Set("decode", js.FuncOf(decode))
-    <-c
+	c := make(chan struct{}, 0)
+	js.Global().Set("encode", js.FuncOf(encode))
+	js.Global().Set("decode", js.FuncOf(decode))
+	<-c
 }
